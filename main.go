ccaa72d11f1967f0f91525471a13b69c29230c4d
Almaz Fedorov <fedoro3valmaz@gmail.com>
2025-07-16 00:12:07 +0300

    7.4. Упражнение Утилита закладок Map

--- main.go
+++ main.go
@@ -3,8 +3,69 @@ package main
 import "fmt"
 
 func main() {
-	m := map[string]int{"a": 1, "b": 2}
-	for key, value := range m {
-		fmt.Println(key, value)
+	bookmarks := map[string]string{}
+	// fmt.Println("1. Посмотреть закладки")
+	// fmt.Println("2. Добавить закладку")
+	// fmt.Println("3. Удалить закладку")
+	// fmt.Println("4. Выход")
+	var input int
+	for {
+		input = getMenu()
+		// fmt.Print("Выберите действие: ")
+		// fmt.Scan(&input)
+		switch input {
+		case 1:
+			showBookmarks(bookmarks)
+		case 2:
+			bookmarks = addBookmark(bookmarks)
+		case 3:
+			deleteBookmark(bookmarks)
+		case 4:
+			fmt.Println("Выход из программы")
+			return
+		}
 	}
+
+}
+
+func showBookmarks(bookmarks map[string]string) {
+	if len(bookmarks) == 0 {
+		fmt.Println("Нет закладок для отображения")
+	}
+	for key, value := range bookmarks {
+		fmt.Println(key, ":", value)
+	}
+
+}
+
+func addBookmark(bookmarks map[string]string) map[string]string {
+	var newBookmarkKey string
+	var newBookmarkValue string
+	fmt.Println("Добавление закладки")
+	fmt.Println("Введите название закладки:")
+	fmt.Scan(&newBookmarkKey)
+	fmt.Println("Введите URL закладки:")
+	fmt.Scan(&newBookmarkValue)
+	bookmarks[newBookmarkKey] = newBookmarkValue
+	return bookmarks
+}
+
+func deleteBookmark(bookmarks map[string]string) map[string]string {
+	var name string
+	fmt.Println("Удаление закладки")
+	fmt.Println("Введите название закладки для удаления:")
+	fmt.Scan(&name)
+	delete(bookmarks, name)
+	return bookmarks
+}
+
+func getMenu() int {
+	var input int
+	fmt.Println("Выберите действие: ")
+	fmt.Println("1. Посмотреть закладки")
+	fmt.Println("2. Добавить закладку")
+	fmt.Println("3. Удалить закладку")
+	fmt.Println("4. Выход")
+	fmt.Scan(&input)
+	return input
 }
