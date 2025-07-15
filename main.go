64e7f3bd81c7e2d4a80e7d32e6d89a0b3e83db7b
Almaz Fedorov <fedoro3valmaz@gmail.com>
2025-07-15 22:39:32 +0300

    7.2. Изменение Map Map

--- main.go
+++ main.go
@@ -7,5 +7,14 @@ func main() {
 		"PurpleSchool": "https://www.purpleschool.com",
 	}
 	fmt.Println(m)
+	fmt.Println(m["PurpleSchool"])
+	m["PurpleSchool"] = "https://www.purpleschool.ru"
+	fmt.Println(m)
+	m["Google"] = "https://www.google.com"
+	fmt.Println(m)
+	m["Yandex"] = "https://www.yandex.ru"
+	fmt.Println(m)
+	delete(m, "Yandex")
+	fmt.Println(m)
 
 }
